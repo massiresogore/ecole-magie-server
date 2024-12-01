2a131f6b00fada41fbd09b53b2ef4b078af60f79
massire <massire.org@gmail.com>
2024-12-01 12:54:23 +0100

    ajout exception si pas connecte

--- src/main/java/emcer/cg/fr/gestionutilisateuronline/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/emcer/cg/fr/gestionutilisateuronline/system/exception/ExceptionHandlerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.validation.FieldError;
@@ -50,6 +51,13 @@ public class ExceptionHandlerAdvice {
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     Result handleAuthenticationException(Exception exception) {
         return new Result(false,"username or password is incorrect",  StatusCode.UNAUTHORIZED,exception.getMessage());
+    }
+
+    /*Nor Login Exception*/
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    Result handleInsufficientAuthenticationException(InsufficientAuthenticationException ex) {
+        return new Result(false,"Login credentials are missing.", StatusCode.UNAUTHORIZED,  ex.getMessage());
     }
 
     @ExceptionHandler(AccountStatusException.class)
